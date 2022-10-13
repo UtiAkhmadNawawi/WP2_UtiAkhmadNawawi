e638732069d942ab95d053c24e958d84db50f74a
UtiAkhmadNawawi <Utiakhmadnawawi110@gmail.com>
2022-10-13 15:00:27 +0700

    Pertemuan 4 membuat template sederhana

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -13,5 +13,22 @@ class Matakuliah extends CI_Controller
     'sks' => $this->input->post('sks')
     ];
     $this->load->view('view-data-matakuliah', $data);
+    $this->form_validation->set_rules('nama', 'nama_matakuliah', 'required|min_lenght[3]',[
+        'required' => 'nama matakuliah harus di isi',
+        'min_lenght' => 'nama terlalu pendek'
+    ]);
+
+    if ($this->form_validation->run() != true;) {
+        $this->load->view('view-form-matakuliah');
+    }
+
+    else {
+        $data = [
+            'kode' = $this->input->post('kode'),
+            'nama' = $this->input->post('nama'),
+            'sks'  = $this->input->post('sks')
+        ];
+    }
+
     }
 }
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama depan : Uti </li>
+        <li>Nama belakang : Akhmad Nawawi</li>
+    </ul>
+    <br>
+    <h4>Almat</h4>
+    <ul type="none">
+        <li> cluster green kramat jati no. 11</li>
+    </ul>
+    <br>
+    <ul type="none">
+        <li>Banyumas</li>
+    </ul>
+    <br>
+    <h4>Olahraga Favorite</h4>
+    <ul type="square">
+        <li>bulu tangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
